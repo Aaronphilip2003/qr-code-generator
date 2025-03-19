a5a7bd5c239bab1d43b13ebc0fa339ea30fe16f9
Aaron <i-aaron.philip@devrev.ai>
2025-03-19 11:04:24 +0530

    adsense crawler fix

---
+++ src/app/ads.txt/route.ts
@@ -0,0 +1,7 @@
+export async function GET() {
+    return new Response('google.com, pub-2976969141373216, DIRECT, f08c47fec0942fa0', {
+        headers: {
+            'Content-Type': 'text/plain',
+        },
+    });
+} 
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,11 +29,3 @@ export default function RootLayout({
     </html>
   );
 }
-
-export async function GET() {
-  return new Response('google.com, pub-2976969141373216, DIRECT, f08c47fec0942fa0', {
-    headers: {
-      'Content-Type': 'text/plain',
-    },
-  });
-}
